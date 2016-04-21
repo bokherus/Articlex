40a5a98ede4916f15e9d71ba869d1bbb191dd8be
Thongrapee <kulaspa@gmail.com>
2016-04-21 07:44:23 +0700

    Fix article not scrolling to top on view

--- js/controllers/ArticleController.js
+++ js/controllers/ArticleController.js
@@ -3,4 +3,9 @@ app.controller('ArticleController', ['$scope', 'articles', '$routeParams',
     articles.success(function(data) {
     $scope.detail = data[$routeParams.id];
   });
+
+  $scope.$on('$routeChangeSuccess',function() {
+     console.log("Scrolled to the top of the article");
+     window.scrollTo(0, 0);
+  });
 }]);
