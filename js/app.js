77b91f93c79580ce693b2e0e5d0894bc256c0b6c
Thongrapee <kulaspa@gmail.com>
2016-04-25 11:31:34 +0700

    Initial transition to Angular Material

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module("myApp", ['ngRoute', 'ngMaterial']);
 
 app.config(function ($routeProvider) {
   $routeProvider
@@ -13,6 +13,39 @@ app.config(function ($routeProvider) {
     .otherwise({
       redirectTo: '/'
     });
+});
+
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('amber')
+    .accentPalette('orange');
+});
+
+app.controller('BasicDemoCtrl', function DemoCtrl($mdDialog) {
+    var originatorEv;
+    this.openMenu = function($mdOpenMenu, ev) {
+      originatorEv = ev;
+      $mdOpenMenu(ev);
+    };
+    this.notificationsEnabled = true;
+    this.toggleNotifications = function() {
+      this.notificationsEnabled = !this.notificationsEnabled;
+    };
+    this.redial = function() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .targetEvent(originatorEv)
+          .clickOutsideToClose(true)
+          .parent('body')
+          .title('Suddenly, a redial')
+          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
+          .ok('That was easy')
+      );
+      originatorEv = null;
+    };
+    this.checkVoicemail = function() {
+      // This never happens.
+    };
 });
 
 $(document).ready(function() {
