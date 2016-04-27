8df21039e6acefa05450d50745e94220722f6b4f
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-04-27 20:56:36 +0700

    access-control-allow-origin *'

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(session({secret: 'secret strategic abcde code'}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next){
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
