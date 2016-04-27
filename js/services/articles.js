a00db87188c8f15bf32169230a1010c93dae120a
Thongrapee <kulaspa@gmail.com>
2016-04-27 21:35:00 +0700

    Modify page to get data from API and show article content via ng-bind-html

--- js/services/articles.js
+++ js/services/articles.js
@@ -1,9 +1,13 @@
 app.factory('articles', ['$http', function($http) {
-  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json')
+  return $http.get('http://chinnnoo.xyz:8889/api/article/latest')
          .success(function(data) {
+           console.log("Success");
            return data;
          })
          .error(function(data) {
+           console.log("Failed");
            return data;
          });
 }]);
+
+// https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json
