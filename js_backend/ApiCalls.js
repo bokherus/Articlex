c043dc2b703e445d81656693b06e0eae7f582503
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-05-01 16:19:33 +0700

    update api

--- js_backend/ApiCalls.js
+++ js_backend/ApiCalls.js
@@ -3,7 +3,7 @@ var Schema = require('./Schema');
 
 
 var getArticle = function(id, callback){
-  var q = 'SELECT ' + Schema.Article.column.id + ', ' + Schema.User.column.username + ' as author ,' + Schema.Article.column.content + ', ' + Schema.Article.column.time + ', ' + Schema.Article.column.title + ' ' +
+  var q = 'SELECT ' + Schema.Article.column.id + ', ' + Schema.User.column.username + ' as author ,' + Schema.Article.column.content + ', ' + Schema.Article.column.time + ', ' + Schema.Article.column.title + ', ' + Schema.ArticleImage.column.url + ' ' +
           'FROM ' + Schema.Article.table + ', ' + Schema.User.table + ' ' +
           'WHERE ' + Schema.Article.column.id + '=' + id + ' AND ' + Schema.Article.column.authorId + '=' + Schema.User.column.commentId + ';';
   console.log(q);
@@ -12,15 +12,16 @@ var getArticle = function(id, callback){
 };
 
 var getLatestArticles = function(callback){
-  var q = 'SELECT articleId, username as author, content, createTime, title FROM ' + Schema.Article.table + ', ' + Schema.User.table +
-          ' WHERE ' + Schema.Article.table + '.' + Schema.Article.column.authorId + ' =' + ' ' + Schema.User.table + '.' + Schema.User.column.id +
-          ' ORDER BY ' + Schema.Article.column.time + ' DESC;';
+  var q = 'SELECT articleId, username as author, content, createTime, title' + ' ' +
+          'FROM ' + Schema.Article.table + ', ' + Schema.User.table + ' ' +
+          'WHERE ' + Schema.Article.table + '.' + Schema.Article.column.authorId + '=' + Schema.User.table + '.' + Schema.User.column.id + ' ' +
+          'ORDER BY ' + Schema.Article.column.time + ' DESC;';
   console.log(q);
   DbEditor.rawQuery(q, callback);
 };
 
 var getLove = function(articleId, callback){
-  var q = 'SELECT ' + Schema.Love.column.uid + ', concat(' + Schema.User.column.fname + ', ' + toString(' ') + ', ' + Schema.User.column.lname +  ') as fullname ' +
+  var q = 'SELECT ' + Schema.Love.column.uid + Schema.User.column.fname + ', ' + Schema.User.column.lname + ' ' +
           'FROM ' + Schema.User.table + ' ' +
           'WHERE ' + Schema.Love.table + '.' + Schema.Love.column.aid + '=' + articleId + ' AND ' + Schema.Article.table + '.' + Schema.Love.column.uid + '=' + Schema.User.table +'.' + Schema.User.column.id +';';
   console.log(q);
@@ -74,7 +75,7 @@ var getTagInArticle = function(articleId, callback){
 };
 
 var getTaggedArticles = function(tagName, callback){
-  var q = 'SELECT ' + Schema.Article.column.id + ', ' + Schema.Article.column.title + ', ' + Schema.Article.column.content + ', ' + 'date_format(' + Schema.Article.column.time + ', \'%e-%c-%Y %T\') as createTime ' +
+  var q = 'SELECT ' + Schema.Article.column.id + ', ' + Schema.Article.column.title + ', ' + Schema.Article.column.content + ', ' + Schema.Article.column.time + ' ' +
           'FROM ' + Schema.Article.table + ', ' + Schema.Tag.table + ' ' +
           'WHERE ' + Schema.Tag.column.tagName + '=' + tagName + ' AND ' + Schema.Tag.column.articleId + '=' + Schema.Article.column.id;
   console.log(q);
