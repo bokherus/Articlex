29d015ca673bb4771459af84dea5ab05cd45489f
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-05-01 15:27:08 +0700

    update from deploy branch

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,9 +1,16 @@
-app.controller('MainController', ['$scope', 'articles',
-function($scope, articles) {
+app.controller('MainController',[
+  '$scope',
+  '$http',
+  'articles',
+  'userinfo',
+  function($scope, $http, articles, userinfo) {
+
   articles.success(function(data) {
     $scope.articles = data;
     $scope.userImage = "https://image.freepik.com/free-icon/user-male-shape-in-a-circle--ios-7-interface-symbol_318-39025.png";
     $scope.loves = 50;
+    console.log(userinfo.getInfo());
+
   });
 
   $scope.$on('$viewContentLoaded', function(){
